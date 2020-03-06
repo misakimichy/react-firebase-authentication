ad75a8a5d5ad064d784eaa38a6532b9ca6deb8b5
Misaki Koonce <misaki.koonce@gmail.com>
2020-03-05 18:32:51 -0800

    Switch layout if authUser exists.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import * as ROUTES from './../../constants/routes'
+import SignOutButton from './../SignOut'
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
     <nav>
-        <ul>
-            <li><Link to={ROUTES.SIGN_IN}>Sign In</Link></li>
-            <li><Link to={ROUTES.LANDING}>Landing</Link></li>
-            <li><Link to={ROUTES.HOME}>Home</Link></li>
-            <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
-            <li><Link to={ROUTES.ADMIN}>Admin</Link></li>
-        </ul>
+       {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
     </nav>
+)
+
+const NavigationAuth = () => (
+    <ul>
+        <li><Link to={ROUTES.SIGN_IN}>Sign In</Link></li>
+        <li><Link to={ROUTES.LANDING}>Landing</Link></li>
+        <li><Link to={ROUTES.HOME}>Home</Link></li>
+        <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
+        <li><SignOutButton /></li>
+    </ul>
+)
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li><Link to={ROUTES.LANDING}>Landing</Link></li>
+        <li><Link to={ROUTES.SIGN_IN}>Sign In</Link></li>
+
+    </ul>
 )
 
 export default Navigation
