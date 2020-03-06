382bba48ffc392e9640e980fa440a565681e44ca
Misaki Koonce <misaki.koonce@gmail.com>
2020-03-05 19:48:04 -0800

    Add PasswordForgetFrom and PasswordChangeFrom in Account component.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { PasswordForgetForm } from './../PasswordForget'
+import PasswordChangeForm from './../PasswordChange'
 
-const Account = () => (
-    <div>Account</div>
+const AccountPage = () => (
+    <div>
+        <h1>Account Page</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+    </div>
 )
 
-export default Account
+export default AccountPage
